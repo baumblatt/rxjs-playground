33fad335088c0d26be330618989592d9029e5004
Bernardo Baumblatt <baumblatt@gmail.com>
2018-01-07 02:38:13 -0200

    organize imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,6 @@ import 'rxjs/add/observable/empty';
 import 'rxjs/add/observable/from';
 import 'rxjs/add/observable/interval';
 import 'rxjs/add/observable/of';
-import 'rxjs/add/observable/zip';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/take';
 
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {Observable} from 'rxjs/Observable';
