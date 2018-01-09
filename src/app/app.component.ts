37de3e67c54f1df1b65979079844fbd3927ec6af
Bernardo Baumblatt <baumblatt@gmail.com>
2018-01-09 02:50:06 -0200

    The utils console, producer and consumer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,47 @@
-import {Component, OnInit} from '@angular/core';
-
-import {BehaviorSubject} from 'rxjs/BehaviorSubject';
-
+import {Component} from '@angular/core';
 import {empty} from 'rxjs/observable/empty';
-import {from} from 'rxjs/observable/from';
-import {interval} from 'rxjs/observable/interval';
-import {of} from 'rxjs/observable/of';
+
+import {Console} from './utils/console';
+import {Consumer} from './utils/consumer';
+import {Producer} from './utils/producer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   /**
-   * The output like console.log.
+   * The debug console.
    */
-  output$ = new BehaviorSubject<String>('');
+  console = new Console();
 
   /**
-   * A simple observer that output events, error and complete.
+   * The producer.
    */
-  observer: any;
+  producer = new Producer();
+
+  /**
+   * The consumer.
+   */
+  consumer: Consumer;
 
   constructor() {
-    // the simple observer implementation
-    this.observer = {
-      next: next => this.println(next),
-      error: error => this.println(error),
-      complete: () => this.println('complete')
-    };
-  }
-
-  ngOnInit() {
-    // this.run();
+    // create the consumer
+    this.consumer = new Consumer('consumer', this.console);
   }
 
   run() {
     // Clear the output
-    this.output$.next('');
+    this.console.clear();
 
-    // Empty observable: [|]
-    const empty$ = empty();
-    // Of observable: [1|]
-    const of$ = of(1);
-    // From observable: [1234|]
-    const from$ = from([1, 2, 3, 4]);
-    // Interval observable: [-1-2-3-4-5-6...]
-    const interval$ = interval(1000);
+    // an empty observable
+    const observable$ = empty();
 
     // subscribe
-    const subscription = empty$.subscribe(this.observer);
+    const subscription = observable$.subscribe(this.consumer);
 
     // unsubscribe
-    // setTimeout(() => subscription.unsubscribe(), 5000);
-  }
-
-  println(string) {
-    this.output$.next(this.output$.value + string + '\n');
+    setTimeout(() => subscription.unsubscribe(), 5000);
   }
 }
